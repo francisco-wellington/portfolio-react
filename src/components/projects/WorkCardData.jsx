27f454f1a1b94ac6b8666188312f5775672a2410
WellingtonSilva12 <wellingtonsilva180g@gmail.com>
2025-09-08 21:24:28 -0300

    Update project url

--- src/components/projects/WorkCardData.jsx
+++ src/components/projects/WorkCardData.jsx
@@ -15,57 +15,57 @@ const ProjectCardData = [
     imgsrc: pro3,
     title: 'Controle de despesas e receitas',
     text: 'Crud desenvolvido para controle financeiro, entrada e saida. Nesse projeto usei as seguintes tecnologias: HTML, CSS, Javascript e Firebase. Com diversas dificuldades e muita pesquisa, conseguir concluir. As minhas principais dificuldades foi a integração de banco de dados noSQL, pois tive pouco contato com a ferramenta, mas com algumas pesquisas conseguir chegar no objetivo.',
-    view: 'https://wellingtonsilva12.github.io/expense-and-income-control/',
+    view: 'https://francisco-wellington.github.io/expense-and-income-control/',
     source: 'https://github.com/nwell-dev/expense-and-income-control'
   },
   {
     imgsrc: pro4,
     title: 'Tela de Login e Cadastro',
     text: 'Tela de login e cadastro, tipo mobile, utilizando os conceitos já adquiridos durante meus estudos. Neste projeto, meu maior desafio foi desenvolver a lógica de transição de tela e adicionar o botão de ação que mostra a senha do usuário, ambos utilizando javascript. Neste projeto fiz uso do pré-processador de css, Sass. Isso tornou o css do projeto bem mais compreensível e fácil de realizar manutenção no código',
-    view: 'https://wellingtonsilva12.github.io/login-and-signup/',
-    source: 'https://github.com/WellingtonSilva12/login-and-signup'
+    view: 'https://francisco-wellington.github.io/login-and-signup/',
+    source: 'https://github.com/francisco-wellington/login-and-signup'
   },
   {
     imgsrc: pro5,
     title: 'Buscador de CEP - Javascript - API',
     text: 'Buscardor de Endereço por CEP, esse projeto usei uma API REST de consulta de CEP, onde o usuário coloca o CEP, e o aplicativo pesquisa e retorna um endereço para o usuário. Neste projeto, tive algumas dificuldades com o javascript, especificamente em tratamento de error. Mas com algumas pesquisas e estudo, conseguir tratar alguns erros no aplicativo. No entanto, aprendi vários conceito sobre o fetch.',
-    view: 'https://wellingtonsilva12.github.io/zip-code-finder/',
+    view: 'https://francisco-wellington.github.io/zip-code-finder/',
     source: 'https://github.com/nwell-dev/zip-code-finder'
   },
   {
     imgsrc: pro6,
     title: 'Agencia Digital',
     text: 'Este é um layout de um site, que achei no uistore, onde a proposta era de criar um site responsivo, pra uma agencia de publicidade, com foco no crecimento das empresas clientes. ',
-    view: 'https://wellingtonsilva12.github.io/Project-Agency/',
-    source: 'https://github.com/WellingtonSilva12/Project-Agency'
+    view: 'https://francisco-wellington.github.io/Project-Agency/',
+    source: 'https://github.com/francisco-wellington/Project-Agency'
   },
   {
     imgsrc: pro2,
     title: 'Calculadora de Blocos',
     text: 'Esta éuma calculadora, que realiza calculos baseado na empresa na qual trabalhei, onde o objetivo era de calcular, a numeração dos blocos numerado.',
-    view: 'https://wellingtonsilva12.github.io/CalculadoraBlocoAzul/',
-    source: 'https://github.com/WellingtonSilva12/CalculadoraBlocoAzul'
+    view: 'https://francisco-wellington.github.io/CalculadoraBlocoAzul/',
+    source: 'https://github.com/francisco-wellington/CalculadoraBlocoAzul'
   },
   {
     imgsrc: pro7,
     title: 'Lista de Tarefas - React',
     text: 'Projeto criado para expandir, testar e desenvolver ainda mais meus conhecimentos de reactjs. E desta vez usei um banco de dados não relacional, que me ajudou a criar, editar e excluir as tarefas. ',
     view: 'https://fastidious-semifreddo-4c1f31.netlify.app',
-    source: 'https://github.com/WellingtonSilva12/task-reactjs'
+    source: 'https://github.com/francisco-wellington/task-reactjs'
   },
   {
     imgsrc: pro8,
     title: 'Links Redes Sociais',
     text: 'Como o uso do linktree é bem limitado no modo free, a empresa decidiu, criar o seu próprio modelo de links, para colocar na bio do instagram, sendo que a mesma é limitada para apenas um link. ',
     view: 'https://dancing-crisp-ee6b2a.netlify.app',
-    source: 'https://github.com/WellingtonSilva12/nobre-links'
+    source: 'https://github.com/francisco-wellington/nobre-links'
   },
   {
     imgsrc: pro9,
     title: 'Sticky Notes',
     text: 'Este é um modelo de CRUD, baseado em bloco de notas adesivas, criado usando html, css e javascript puro, quando banco de dados localStorage',
-    view: 'https://wellingtonsilva12.github.io/sticky-notes/',
-    source: 'https://github.com/WellingtonSilva12/sticky-notes'
+    view: 'https://francisco-wellington.github.io/sticky-notes/',
+    source: 'https://github.com/francisco-wellington/sticky-notes'
   },
   {
     imgsrc: pro11,
@@ -78,8 +78,8 @@ const ProjectCardData = [
     imgsrc: pro12,
     title: 'Tradutor',
     text: 'Projeto criado, utilizando a API MyMemory Translated. O intuito desse projeto é de traduzir os textos das principais línguas mais falada do mundo',
-    view: 'https://wellingtonsilva12.github.io/translate-js/',
-    source: 'https://github.com/nwell-dev/translate-js'
+    view: 'https://francisco-wellington.github.io/translate-js/',
+    source: 'https://github.com/francisco-wellington/translate-js'
   }
 ]
 
